e09b9c6c5cc48cc0a005b18fe64c783aa961d0be
Serhii <vasylyk29@gmail.com>
2020-04-16 12:22:32 +0300

    Updates

---
+++ precache-manifest.aad8cbf922c05ba55c2cfb51069e3a96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac7466d47303f9bb4849c77cc439f9a",
+    "url": "/cursor_react_jss/index.html"
+  },
+  {
+    "revision": "072b3f09fbb5b3bc075c",
+    "url": "/cursor_react_jss/static/css/main.dceecaf1.chunk.css"
+  },
+  {
+    "revision": "e5e01302691c56715114",
+    "url": "/cursor_react_jss/static/js/2.09fd4f5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor_react_jss/static/js/2.09fd4f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072b3f09fbb5b3bc075c",
+    "url": "/cursor_react_jss/static/js/main.4a6c1d60.chunk.js"
+  },
+  {
+    "revision": "b05d277825b4e79873ae",
+    "url": "/cursor_react_jss/static/js/runtime-main.d14cf54c.js"
+  },
+  {
+    "revision": "08aa73b53a8862635b811eb4c60656db",
+    "url": "/cursor_react_jss/static/media/home_bg.08aa73b5.jpg"
+  }
+]);
